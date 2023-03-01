74d8d4c6023a813a0de3df7010af2f8b82657e95
Edison Loor <edison.david.loor@gmail.com>
2023-03-01 15:10:22 -0600

    IIFE added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-let pokemonList = [
-    { name: "bulbasaur", height: 7, type: ["grass", "poison"] },
-    { name: "ivysaur", height: 1, type: ["grass", "poison"] },
-    { name: "venusaur", height: 0.3, type: ["grass", "poison"] },
-    { name: "Groudon", height: 3.5, type: ["ground"] },
-    { name: "charmander", height: 2.9, type: ["fire"] },
-    { name: "charmeleon", height: 0.6, type: ["fire"] },
-];
-for (let i = 0; i < pokemonList.length; i++)
-// Initalize for loop to run for the total length of the array
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: "bulbasaur", height: 7, type: ["grass", "poison"] },
+        { name: "ivysaur", height: 1, type: ["grass", "poison"] },
+        { name: "venusaur", height: 0.3, type: ["grass", "poison"] },
+        { name: "Groudon", height: 3.5, type: ["ground"] },
+        { name: "charmander", height: 2.9, type: ["fire"] },
+        { name: "charmeleon", height: 0.6, type: ["fire"] },
+    ];
 
-{
-    if (pokemonList[i].height > 1) {
-        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")  - Wow, that's big!" + "<br>");
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
 
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+function printArrayDetails(i){
+    if (i.height > 0.6){
+        document.write(i.name + ' (height: ' + i.height + ')' +  " - Wow, that's big!"  + '<br>')
+    }
     else {
-        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+        document.write(i.name + ' (height: ' + i.height + ')' + '<br>')
     }
 }
+pokemonRepository.getAll().forEach(printArrayDetails);
