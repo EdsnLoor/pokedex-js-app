7bf460ddacb811f06689a7f2d265d39d69f79a63
Edison Loor <edison.david.loor@gmail.com>
2023-03-05 20:23:49 -0600

    DOM interaction added

--- js/script.js
+++ js/script.js
@@ -1,33 +1,49 @@
 let pokemonRepository = (function () {
     let pokemonList = [
-        { name: "bulbasaur", height: 7, type: ["grass", "poison"] },
-        { name: "ivysaur", height: 1, type: ["grass", "poison"] },
-        { name: "venusaur", height: 0.3, type: ["grass", "poison"] },
+        { name: "Bulbasaur", height: 7, type: ["grass", "poison"] },
+        { name: "Ivysaur", height: 1, type: ["grass", "poison"] },
+        { name: "Venusaur", height: 0.3, type: ["grass", "poison"] },
         { name: "Groudon", height: 3.5, type: ["ground"] },
-        { name: "charmander", height: 2.9, type: ["fire"] },
-        { name: "charmeleon", height: 0.6, type: ["fire"] },
+        { name: "Charmander", height: 2.9, type: ["fire"] },
+        { name: "Charmeleon", height: 0.6, type: ["fire"] },
     ];
 
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
 
+    // Get all the Pokémon
     function getAll() {
         return pokemonList;
+    }
+    function addListItem(pokemon){
+        // setting variables
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        let button = document.createElement("button");
+
+        // creating a button for each Pokémon
+        button.innerText = pokemon.name;
+        button.classList.add("button-class")
+        listPokemon.appendChild(button)
+        pokemonList.appendChild(listPokemon)
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+    }
+
+    // Shows Pokémon individual details
+    function showDetails(pokemon) {
+        console.log(pokemon)
     }
 
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem : addListItem
     };
 })();
 
-function printArrayDetails(i){
-    if (i.height > 0.6){
-        document.write(i.name + ' (height: ' + i.height + ')' +  " - Wow, that's big!"  + '<br>')
-    }
-    else {
-        document.write(i.name + ' (height: ' + i.height + ')' + '<br>')
-    }
-}
-pokemonRepository.getAll().forEach(printArrayDetails);
+pokemonRepository.getAll().forEach(function (pokemon){
+    pokemonRepository.addListItem(pokemon);
+});
