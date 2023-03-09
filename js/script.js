f8c0b1d92a4404d3da8d1e7f22bd3b37a46eeb94
Edison Loor <edison.david.loor@gmail.com>
2023-03-09 16:49:06 -0600

    bug fixed

--- js/script.js
+++ js/script.js
@@ -81,6 +81,7 @@ let pokemonRepository = (function () {
         loadList(pokemon).then(function () {
             console.log(pokemon);
         });
+        hideLoadingMessage();
     }
 
     function showLoadingMessage() {
